aac189c0b64cbb4aa1b63f07659d4161b0640f98
zakaria <zakariaazaraf@gmail.com>
2021-09-11 18:54:38 +0100

    add the inline style using object

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const Book = () =>{
 }
 
 const Image = () => <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Dostoevskij_1863.jpg/400px-Dostoevskij_1863.jpg' alt='Title'/>
-const Title = () => <h2>The book title</h2>
+const Title = () => <h2 style={{color: 'blue', fontSize: '2rem'}}>The book title</h2>
 const Author = () => <h4>Zakaria Azaraf</h4>
 
 ReactDom.render(
