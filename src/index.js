e3ed0593dc7a775772f57a1b0bd0de9941337737
zakaria <zakariaazaraf@gmail.com>
2021-09-11 19:04:28 +0100

    add some content using variable and jsx

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+// Set up some variable 
+let imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Dostoevskij_1863.jpg/400px-Dostoevskij_1863.jpg'
+let author = 'Zakaria Azaraf'
+
 const BookList = ()=>{
   return <section>
     <Book />
@@ -21,9 +25,9 @@ const Book = () =>{
   </article>
 }
 
-const Image = () => <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Dostoevskij_1863.jpg/400px-Dostoevskij_1863.jpg' alt='Title'/>
+const Image = () => <img src={imgSrc} alt='Title'/>
 const Title = () => <h2 style={{color: 'blue', fontSize: '2rem'}}>The book title</h2>
-const Author = () => <h4>Zakaria Azaraf</h4>
+const Author = () => <h4>{author}</h4>
 
 ReactDom.render(
   <React.StrictMode>
