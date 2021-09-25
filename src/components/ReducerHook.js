a0dea3f205475bbcf741d437203b84056060a3dd
zakaria <zakariaazaraf@gmail.com>
2021-09-25 19:27:24 +0100

    Added the reducer method to handle state and added persons

--- src/components/ReducerHook.js
+++ src/components/ReducerHook.js
@@ -1,21 +1,54 @@
 import React , { useState, useReducer } from 'react'
 import {data} from '../data'
 
+const Modle = ({ modleContent})=>{
+    // return <h2>{modleContent}</h2>
+    return <h2>Hello world</h2>
+}
+
 const ReducerHook = () => {
     const [name, setName] = useState('')
-    const [people, setPeople] = useState(data)
+    // const [people, setPeople] = useState(data)
+
+    // Reducer functtion
+    const reducer = (state, action)=>{
+        // You always need to return a state
+        if(action.type === 'TESTING'){
+            return {
+                // i don't understand wuy the constructor write this destructring
+                // ...state,
+                people: [...state.people, {id: new Date().getTime().toString(), name}],
+                isModleOpen: true,
+                modleContent: 'Person added'
+            }
+        }
+        return state
+    }
+
+    const initialState = {
+        people: data,
+        isModleOpen: false,
+        modleContent: ''
+    }
+    // Call the reducer hook
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     const handleSubmitForm = (e)=>{
         e.preventDefault()
+        if(name){
+            dispatch({type:'TESTING'})  
+            setName('')   
+        }
     }
     return <>
+        {state.isModleOpen && <Modle />}
         <form onSubmit={handleSubmitForm}>
             <label htmlFor='name'>Name:</label>
-            <input type='text' id='name' name='name' value={name} onChange={()=> setName(name)}></input>
+            <input type='text' id='name' name='name' value={name} onChange={(e)=>setName(e.target.value)}></input>
             <button type='submit'>Add Person</button>
         </form>
         {
-            people.map(person =>{
+            state.people.map(person =>{
                 return <article key={person.id}>
                     <h2>{person.name}</h2>
                 </article>
