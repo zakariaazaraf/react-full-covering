c8c3653358b3b01b98af1a53e907fdf29519fa38
zakaria <zakariaazaraf@gmail.com>
2021-10-03 13:04:20 +0100

    passed the functtion that removes single user trought multiple component levels

--- src/components/PropDrilling.js
+++ src/components/PropDrilling.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { useState } from 'react/cjs/react.development'
 import {data} from '../data'
 
-const Lists = ({ people })=>{
+const Lists = ({ people, removePerson })=>{
     return <>
         <section className='persons'>
             {people.map(person =>{
-                return <List person={person}/>
+                return <List person={person} removePerson={removePerson}/>
             })}
         </section>
     </>
 }
 
-const List = ({ person })=>{
+const List = ({ person, removePerson })=>{
     const { id } = person
     return <>
         <article key={id} className='person'>
             <Person person={person}/>
-            <RemovePerson id={id} />
+            <RemovePerson id={id} removePerson={removePerson}/>
         </article>
     </>
 }
@@ -28,17 +28,22 @@ const Person = ({ person })=>{
         </>
 }
 
-const RemovePerson = ()=>{
+const RemovePerson = ({ id, removePerson })=>{
     return<>
-        <button className='btn-delete' >remove</button>
+        <button className='btn-delete' onClick={()=>removePerson(id)}>remove</button>
     </>;
 }
 
 const PropDrilling = () => {
 
     const [people, setPeople] = useState(data)
+
+    const removePerson = (id)=>{
+        setPeople(people.filter(person => person.id !== id))
+    }
     return <>
-     <Lists people={people} />
+    <h1>Prop Drilling</h1>
+     <Lists people={people} removePerson={removePerson}/>
     </>
 }
 
